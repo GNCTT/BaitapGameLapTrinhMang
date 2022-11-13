0c0ad747efb15d2ad47fb2ae53f0e3cf0d3fde20
GNCTT <70886052+GNCTT@users.noreply.github.com>
2022-11-13 21:22:04 +0700

    hello world

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class GameClient {
+    int arr_map[][];
+
+    public GameClient() {
+
+    }
+
+    public void createMap(int width, int height) {
+        this.arr_map = new int[width][height];
+    }
+
+    public void print() {
+
+    }
     public static boolean checkDiChuyen(int[][] map,int[][] mayBay,int x,int y){
         int h=0,k=0;
         if(x-2<0 || x+2>= map[0].length || y-2<0 || y+2>= map.length){
