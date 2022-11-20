ed52ff0a8996f1c8b087b429cb7b03649ed85333
duychien140501 <Bietlamgi1@>
2022-11-21 00:36:03 +0700

    Chien cm 21/11

---
+++ src/Start/GameOther.java
@@ -0,0 +1,17 @@
+package Start;
+
+public class GameOther {
+    static int width;
+    static int heigth;
+    public static int[][] mapEnermy;
+    public static int[][] trapEnermy;
+
+    static int xEnermy;
+    static int yEnermy;
+    public  GameOther() {
+
+    }
+    public  GameOther(int width, int heigth) {
+
+    }
+}
